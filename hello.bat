77438006bfc9531d2ceff2af4db123038c438ff3
Kian Moretz <meecoder@users.noreply.github.com>
2013-11-01 07:28:41 -0400

    add input

--- hello.bat
+++ hello.bat
@@ -1,5 +1,7 @@
 @echo off
-echo Hi User!
+echo Please type your name followed by Enter, then Ctrl-Z, and then Enter.
+FOR /F "tokens=*" %%A IN ('TYPE CON') DO SET INPUT=%%A
+ECHO Hi %INPUT%!
 pause
 echo I hope you have a good day!
 pause
